1e1a68a1404acc12780a9f7cb6fb7fea2a3a8325
mehdialmasii <mehdialmasi.mehdi@gmail.com>
2025-12-07 11:26:01 +0330

    initial commit

---
+++ x=16.py
@@ -0,0 +1,19 @@
+import random
+def win(pcguess,userguess):
+    return pcguess==userguess
+def randomm_number(x,y):
+    m=random.randint(x,y)
+    return m
+X=int(input("WRITE NUMBER FOR MIN-imum OF YOUR DOMAIN:"))
+Y=int(input("WRITE NUMBER FOR MAX-imum OF YOUR DOMAIN:"))
+usernumber=int(input(f"please write a number between {X} AND {Y} :"))
+compguees=randomm_number(X,Y)
+count=0
+while (not win(compguees,usernumber)):
+    if compguees > usernumber:
+        Y=compguees
+    else:
+        X=compguees    
+    count+=1
+    compguees=randomm_number(X,Y)
+print(f"GOOOOZOOOOOO i found your number in {count} and it is {compguees}")
